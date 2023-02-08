0c07b21998c6640ee4caeabe37705723e7a0449c
Gilson Filho <gilsonfilho1994@yahoo.com.br>
2023-02-08 00:47:10 -0300

    fixing header

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.querySelector('form')
 const nlwSetup = new NLWSetup(form)
 
 const data = {
-    run: ["01-01", "01-02", "01-06"],
+    run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
     takePills: ['01-03'],
     journal: ['01-02']
 }
